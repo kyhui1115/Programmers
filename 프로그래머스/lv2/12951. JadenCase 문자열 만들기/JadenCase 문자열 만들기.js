5b9e0584dd5aaa7a7c2dbfe54c96a6eba89a38d0
kyhui1115 <113227749+kyhui1115@users.noreply.github.com>
2023-04-26 17:57:13 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,5 +1,22 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
 function solution(s) {
     let a = s.split(' ')
+    let e = ''
     return a.map((b)=>{
         let e = ''
         for(let i = 0; i < b.length; i++) {
