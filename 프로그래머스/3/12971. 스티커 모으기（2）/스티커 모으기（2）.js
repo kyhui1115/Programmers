2ec596ade23c3795c231e12a0c5f6877fa96fcd0
kyhui1115 <113227749+kyhui1115@users.noreply.github.com>
2023-11-15 16:42:11 +0900

    [level 3] Title: 스티커 모으기(2), Time: 9.56 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -1,46 +1,19 @@
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
 function solution(sticker) {
-    var answer = 0;
-    var dp1 = [];
-    var dp2 = [];
-    var len = sticker.length;
-
-    if(len <3){
-        return Math.max.apply(null, sticker);
+    var com1=[];
+    var com2=[];
+    if(sticker.length==1)return sticker[0];
+    com1[0]=sticker[0];
+    com1[1]=sticker[0];
+    for(var i=2;i<sticker.length-1;i++){
+        com1[i]=Math.max(com1[i-1],com1[i-2]+sticker[i]);
+    }
+    com2[0]=0;
+    com2[1]=sticker[1];
+    for(var i=2;i<sticker.length;i++){
+        com2[i]=Math.max(com2[i-1],com2[i-2]+sticker[i]);        
     }
 
-    //sticker 1 select
-    dp1[0]=sticker[0];
-    dp1[1]=dp1[0];
-    for(var i=2;i<len-1;i++){
-        dp1[i]= Math.max(dp1[i-1], dp1[i-2]+sticker[i]);
-    }
-    //sticker 2 select
-    dp2[0]=0;
-    dp2[1]=sticker[1];
-    for(var i=2;i<len;i++){
-        dp2[i]= Math.max(dp2[i-1], dp2[i-2]+sticker[i]);
-    }
+    if(com1[com1.length-1]>com2[com2.length-1])return com1[com1.length-1];
+    else return com2[com2.length-1];
 
-    return Math.max(dp1[len-2], dp2[len-1]);
 }
