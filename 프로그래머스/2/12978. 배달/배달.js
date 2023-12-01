098b055f7b5fa62a2bc7e11df4a09a099f4c4c19
kyhui1115 <113227749+kyhui1115@users.noreply.github.com>
2023-12-01 19:19:49 +0900

    [level 2] Title: 배달, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,28 +1,34 @@
 function solution(N, road, K) {
-    const arr = []
-    
-    for(let i = 1; i <= N; i++) {
-        arr[i] = []
-        
-        for(let j = 1; j <= N; j++) {
-            arr[i][j] = i === j ? 0 : Infinity
-        }
+    const arr = [];
+
+    for (let i = 1; i <= N; i++) {
+        arr[i] = [];
     }
-    
+
     road.forEach(([v1, v2, dist]) => {
-        arr[v1][v2] = arr[v1][v2] > dist ? dist : arr[v1][v2]
-        arr[v2][v1] = arr[v2][v1] > dist ? dist : arr[v2][v1]
-    })
-    
-    for(let k = 1; k <= N; k++) {
-        for(let i = 1; i <= N; i++) {
-            for(let j = 1; j <= N; j++) {
-                if(arr[i][j] > arr[i][k] + arr[k][j]) {
-                    arr[i][j] = arr[i][k] + arr[k][j]
-                }
+        arr[v1].push({ to: v2, dist });
+        arr[v2].push({ to: v1, dist });
+    });
+
+    const dist = Array(N + 1).fill(Infinity);
+    dist[1] = 0;
+
+    const queue = [{ to: 1, dist: 0 }];
+
+    while (queue.length) {
+        const { to, dist: currentDist } = queue.shift();
+
+        if (currentDist > dist[to]) continue;
+
+        for (let next of arr[to]) {
+            const newDist = currentDist + next.dist;
+
+            if (newDist < dist[next.to]) {
+                dist[next.to] = newDist;
+                queue.push({ to: next.to, dist: newDist });
             }
         }
     }
-    
-    return arr[1].filter((dist)=> dist <= K).length
+
+    return dist.filter((d) => d <= K).length;
 }
